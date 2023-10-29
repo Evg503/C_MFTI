c907edc3ac53fd947ec62deb2070c7098720fdd2
Evgeny Kochenyuk <evgeny.kochenyuk@gmail.com>
2023-10-30 00:03:59 +0100

    fix mm for memory leaks

--- Seminar3/problem_mm.c
+++ Seminar3/problem_mm.c
@@ -12,11 +12,12 @@ int main() {
     res = scanf("%d", &n);
     assert(res == 1);
 
-    a = calloc(n, sizeof(int));
     if (n <= 0) {
         printf("0 0\n");
         return 0;
     }
+    a = calloc(n, sizeof(int));
+    assert(a != NULL);
 
 
     for (int i = 0; i < n; ++i) {
@@ -27,11 +28,12 @@ int main() {
     {
         int min = a[0];
         int max = a[0];
-        for (int i = 0; i < n; ++i) {
+        for (int i = 1; i < n; ++i) {
             if (min > a[i]) min = a[i];
             if (max < a[i]) max = a[i];
         }
 
         printf("%d %d\n", min, max);
     }
+    free(a);
 }
