d41c37bf79e1dd4f360f1f5f823ce42d3a35ea41
Evgeny Kochenyuk <evgeny.kochenyuk@gmail.com>
2023-10-29 23:44:16 +0100

    Seminar 3 problem mm

---
+++ Seminar3/problem_mm.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+
+
+int main() {
+    int n;
+    int *a;
+
+    int res;
+
+    res = scanf("%d", &n);
+    assert(res == 1);
+
+    a = calloc(n, sizeof(int));
+    if (n <= 0) {
+        printf("0 0\n");
+        return 0;
+    }
+
+
+    for (int i = 0; i < n; ++i) {
+        res = scanf("%d", a + i);
+        assert(res == 1);
+    }
+
+    {
+        int min = a[0];
+        int max = a[0];
+        for (int i = 0; i < n; ++i) {
+            if (min > a[i]) min = a[i];
+            if (max < a[i]) max = a[i];
+        }
+
+        printf("%d %d\n", min, max);
+    }
+}
